683023686955b1d491376810af100644151de138
Brian Fink <bfink13@gmail.com>
2011-12-08 23:20:05 -0600

    Added day picker view controller

---
+++ whoshungry/DayPickerView.h
@@ -0,0 +1,13 @@
+//
+//  DayPickerView.h
+//  whoshungry
+//
+//  Created by Brian Fink on 12/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayPickerView : UIViewController
+
+@end
