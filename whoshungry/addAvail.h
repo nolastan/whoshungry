17df75861dae256b1912881cc5e189f8681a3fed
Stanford Rosenthal <nolastan@gmail.com>
2011-12-08 22:56:33 -0600

    add avail view

--- whoshungry/addAvail.h
+++ whoshungry/addAvail.h
@@ -9,8 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface addAvail : UIViewController{
-    IBOutlet UIPickerView *picker;
+    IBOutlet UIDatePicker *timePicker;
+    IBOutlet UITableView *table;
+    NSString *startTime;
+    NSString *endTime;
+    NSString *days;
+    NSString *notes;
 }
 -(IBAction)cancel:(id)sender;
+
+@property (nonatomic, retain) NSArray *times;
+@property (nonatomic, retain) NSString *startTime;
+@property (nonatomic, retain) NSString *endTime;
+@property (nonatomic, retain) NSString *days;
+@property (nonatomic, retain) NSString *notes;
+@property (nonatomic, retain) IBOutlet UIDatePicker *timePicker;
 
 @end
--- whoshungry/addAvail.m
+++ whoshungry/addAvail.m
@@ -7,14 +7,17 @@
 //
 
 #import "addAvail.h"
+#import "availCell.h"
 
 @implementation addAvail
+@synthesize startTime, endTime, days, notes, timePicker;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
     }
     return self;
 }
@@ -35,8 +38,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [picker setOpaque:NO];
     // Do any additional setup after loading the view from its nib.
+    self.timePicker.hidden=YES;
 }
 
 - (void)viewDidUnload
@@ -50,6 +53,70 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 4;
+}
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"availCell";
+    
+    availCell *cell = (availCell *) [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        NSArray *topLevelObjects = [[NSBundle mainBundle]
+                                    loadNibNamed:@"availCell" owner:nil options:nil];
+        
+        for(id currentObject in topLevelObjects){
+            if([currentObject isKindOfClass:[UITableViewCell class]]){
+                cell = (availCell *) currentObject;
+                break;
+            }
+        }
+    }
+	
+    if([indexPath section] == 0){
+        cell.timeLabel.text = @"Start Time";
+        cell.daysLabel.text = startTime;
+    }
+    if([indexPath section] == 1){
+        cell.timeLabel.text = @"End Time";
+        cell.daysLabel.text = endTime;
+    }
+    if([indexPath section] == 2){
+        cell.timeLabel.text = @"Days";
+        cell.daysLabel.text = days;
+    }
+    if([indexPath section] == 3){
+        cell.timeLabel.text = @"Notes";
+        cell.daysLabel.text = notes;
+    }
+	
+	return cell; 
+}
+
+// Actions 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if([indexPath section] == 0){
+        self.timePicker.hidden = NO;
+    }
+    if([indexPath section] == 1){
+        self.timePicker.hidden = NO;
+    }
+    if([indexPath section] == 2){
+        self.timePicker.hidden = YES;
+    }
+    if([indexPath section] == 3){
+        self.timePicker.hidden = YES;
+    }
+
+	
 }
 
 @end
